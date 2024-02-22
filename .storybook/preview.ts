f5b928b7332d06f27fba0ebe4da7cf6281d9d0fe
Grzegorz Tobiasz <grzegorz.tobiasz97@gmail.com>
2024-02-22 17:37:50 +0100

    Feat: added storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,43 @@
+import type { Preview } from '@storybook/react';
+import '@styles/globals.css';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
+const preview: Preview = {
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	},
+	globalTypes: {
+		theme: {
+			description: 'Global theme for components',
+			defaultValue: 'dark',
+			toolbar: {
+				// The label to show for this toolbar item
+				title: 'Theme',
+				icon: 'circlehollow',
+				// Array of plain string values or MenuItem shape (see below)
+				items: ['light', 'dark'],
+				// Change title based on selected value
+				dynamicTitle: true
+			}
+		}
+	}
+};
+
+export const decorators = [
+	withThemeByDataAttribute({
+		themes: {
+			light: 'light',
+			dark: 'dark'
+		},
+		defaultTheme: 'light',
+		attributeName: 'data-mode'
+	})
+];
+
+export default preview;
---
+++ stories/components/Button.stories.tsx
@@ -0,0 +1,61 @@
+import { CheckIcon } from '@heroicons/react/24/solid';
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from '@components/ui/Button';
+
+const meta = {
+	title: 'Components/Button',
+	component: Button,
+	parameters: {
+		layout: 'centered'
+	},
+	tags: ['autodocs'],
+	argTypes: {
+		children: {
+			control: 'text',
+			defaultValue: 'Test',
+			description: 'Button label'
+		},
+		icon: {
+			options: ['Check', 'None'],
+			mapping: {
+				Check: <CheckIcon />,
+				None: null
+			}
+		}
+	}
+} satisfies Meta<typeof Button>;
+
+export default meta;
+export type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+	args: {
+		children: 'Primary'
+	}
+};
+
+export const Secondary: Story = {
+	args: {
+		children: 'Secondary',
+		variant: 'secondary'
+	}
+};
+
+export const PrimaryWithIcon: Story = {
+	args: {
+		icon: <CheckIcon />
+	}
+};
+
+export const SecondaryWithIcon: Story = {
+	args: {
+		variant: 'secondary',
+		icon: <CheckIcon />
+	}
+};
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+// More on argTypes: https://storybook.js.org/docs/api/argtypes
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
